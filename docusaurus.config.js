3a6170a2f263c8230f8b3ca0553f161a0f05707d
Matheus Diniz <mtsdnz@hotmail.com>
2023-09-15 17:04:46 -0300

    add google analytics tracking

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,6 +46,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: 'G-DWXL1GECSY',
+          anonymizeIP: false,
+        },
       }),
     ],
   ],
